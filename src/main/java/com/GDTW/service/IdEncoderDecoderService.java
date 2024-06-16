02f215522e2dd1f1925d842a94cd0157433f0354
EugenPC-IDEA <hsuanyulin1993@gmail.com>
2024-06-16 16:53:28 +0800

    EugenPC-IDEA 16June2024
    To update "develop" branch:
    1. To add new method for encode/decode of id.

--- src/main/java/com/GDTW/service/IdEncoderDecoderService.java
+++ src/main/java/com/GDTW/service/IdEncoderDecoderService.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class IdEncoderDecoderService {
 
-    private static final String ENCRYPT_CHARS = "XgUweOlDKZlVAYNp4jq3FWMTQoEuP9I5nHbfCy2czv1mkdS60JxRGs78aBhLi";
-    private static final int BASE = ENCRYPT_CHARS.length();
+    private static final String ENCRYPT_CHARS_A = "XgUweOlDKZlVAYNp4jq3FWMTQoEuP9I5nHbfCy2czv1mkdS60JxRGs78aBhLi";
+    private static final String ENCRYPT_CHARS_B = "TyVM7HzaSlXE4mnL8Kowu2hjgqdIliDFG6vpPQOA0cC9Z1sBU35fbkxeNRJYW";
+    private static final int BASE_A = ENCRYPT_CHARS_A.length();
+    private static final int BASE_B = ENCRYPT_CHARS_B.length();
     private static final int MIN_ID = 10000000;
     private static final int MAX_ID = 23500000;
 
@@ -14,13 +16,17 @@ public class IdEncoderDecoderService {
 
     public static String encodeId(Integer id) {
         if (id < MIN_ID || id > MAX_ID) {
-            throw new IllegalArgumentException("ID should between " + MIN_ID + " and " + MAX_ID + ".");
+            throw new IllegalArgumentException("ID should be between " + MIN_ID + " and " + MAX_ID + ".");
         }
         int normalizedId = id - MIN_ID;
         char[] encodedChars = new char[4];
         for (int i = 3; i >= 0; i--) {
-            encodedChars[i] = ENCRYPT_CHARS.charAt(normalizedId % BASE);
-            normalizedId /= BASE;
+            if (i % 2 == 0) {
+                encodedChars[i] = ENCRYPT_CHARS_A.charAt(normalizedId % BASE_A);
+            } else {
+                encodedChars[i] = ENCRYPT_CHARS_B.charAt(normalizedId % BASE_B);
+            }
+            normalizedId /= (i % 2 == 0) ? BASE_A : BASE_B;
         }
         return new String(encodedChars);
     }
@@ -31,7 +37,11 @@ public class IdEncoderDecoderService {
         }
         int id = 0;
         for (int i = 0; i < 4; i++) {
-            id = id * BASE + ENCRYPT_CHARS.indexOf(encoded.charAt(i));
+            if (i % 2 == 0) {
+                id = id * BASE_A + ENCRYPT_CHARS_A.indexOf(encoded.charAt(i));
+            } else {
+                id = id * BASE_B + ENCRYPT_CHARS_B.indexOf(encoded.charAt(i));
+            }
         }
         id += MIN_ID;
         return id;
