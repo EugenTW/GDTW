730df53f68b447b2cdd3c2e8f4872c2c578b17cb
EugenPC-IDEA <hsuanyulin1993@gmail.com>
2024-10-20 22:14:35 +0800

    EugenPC-IDEA 2024Oct20
    To update "develop" branch:
    1. To update new front-end.
    2. To fix small recording bugs in Redis.

--- src/main/java/com/GDTW/WebVersion.java
+++ src/main/java/com/GDTW/WebVersion.java
@@ -2,8 +2,8 @@ package com.GDTW;
 
 public class WebVersion {
 
-    private static final String WebVersion = "1.2.4";
-    private static final String BuildDate = "2024Sep17";
+    private static final String WebVersion = "1.3.0";
+    private static final String BuildDate = "2024Oct20";
 
     public static String getWebVersion() {
         return WebVersion;
