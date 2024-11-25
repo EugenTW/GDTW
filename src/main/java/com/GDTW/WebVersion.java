5b18e1cdf157d86e530f3e781908ed37a68422c9
eugen.lin <eugen.lin@raritan.com>
2024-11-25 09:56:04 +0800

    2024Nov25 1. To update 'Cache' policy.

--- src/main/java/com/GDTW/WebVersion.java
+++ src/main/java/com/GDTW/WebVersion.java
@@ -3,7 +3,7 @@ package com.GDTW;
 public class WebVersion {
 
     private static final String WebVersion = "1.7.0";
-    private static final String BuildDate = "2024Nov18";
+    private static final String BuildDate = "2024Nov25";
 
     public static String getWebVersion() {
         return WebVersion;
