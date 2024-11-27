e349417e09555986e523c59503f4ca030b0a88fc
eugen.lin <eugen.lin@raritan.com>
2024-11-27 13:59:22 +0800

    2024Nov27 - v 1.8.2 1. To update POM.

--- src/main/java/com/GDTW/WebVersion.java
+++ src/main/java/com/GDTW/WebVersion.java
@@ -2,8 +2,8 @@ package com.GDTW;
 
 public class WebVersion {
 
-    private static final String WebVersion = "1.8.1";
-    private static final String BuildDate = "2024Nov26";
+    private static final String WebVersion = "1.8.2";
+    private static final String BuildDate = "2024Nov27";
 
     public static String getWebVersion() {
         return WebVersion;
