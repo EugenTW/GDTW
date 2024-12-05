b0c8775090f00ac210be3a84e56f60e959a4c4c7
eugen.lin <eugen.lin@raritan.com>
2024-12-05 10:32:41 +0800

    2024Dec05 - v 1.8.8

--- src/main/java/com/GDTW/WebVersion.java
+++ src/main/java/com/GDTW/WebVersion.java
@@ -2,8 +2,8 @@ package com.GDTW;
 
 public class WebVersion {
 
-    private static final String WebVersion = "1.8.7";
-    private static final String BuildDate = "2024Dec04";
+    private static final String WebVersion = "1.8.8";
+    private static final String BuildDate = "2024Dec05";
 
     public static String getWebVersion() {
         return WebVersion;
