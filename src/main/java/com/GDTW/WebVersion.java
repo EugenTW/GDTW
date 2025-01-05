9aceb9e477912f8eb0de5ca8734a3c27cd10465b
EugenPC-IDEA <hsuanyulin1993@gmail.com>
2025-01-05 20:00:12 +0800

    EugenPC-IDEA 2025Jan05 v1.10.3

--- src/main/java/com/GDTW/WebVersion.java
+++ src/main/java/com/GDTW/WebVersion.java
@@ -2,7 +2,7 @@ package com.GDTW;
 
 public class WebVersion {
 
-    private static final String WebVersion = "1.10.0";
+    private static final String WebVersion = "1.10.3";
     private static final String BuildDate = "2025Jan01";
 
     public static String getWebVersion() {
