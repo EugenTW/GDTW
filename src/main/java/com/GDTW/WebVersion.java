99946688452660080fe03f47a29e498f01919d34
EugenPC-IDEA <hsuanyulin1993@gmail.com>
2025-03-06 00:40:51 +0800

    EugenPC 2025Mar06 v1.13.4 1. To fix bugs.

--- src/main/java/com/GDTW/WebVersion.java
+++ src/main/java/com/GDTW/WebVersion.java
@@ -2,8 +2,8 @@ package com.GDTW;
 
 public class WebVersion {
 
-    private static final String WebVersion = "1.13.3";
-    private static final String BuildDate = "2025Mar05";
+    private static final String WebVersion = "1.13.4";
+    private static final String BuildDate = "2025Mar06";
 
     public static String getWebVersion() {
         return WebVersion;
