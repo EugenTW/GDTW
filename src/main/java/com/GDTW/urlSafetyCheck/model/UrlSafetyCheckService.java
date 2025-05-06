25348f0bfc1f907f50df6c6f17881ddbeda63c04
eugen.lin <eugen.lin@raritan.com>
2025-05-06 15:21:17 +0800

    2025May06 v1.14.0 1. To update 'Image Share' module. 2. To replace all jQuery code with Vanilla JS for better performance and maintainability. 3. To fix bugs.

--- src/main/java/com/GDTW/urlSafetyCheck/model/UrlSafetyCheckService.java
+++ src/main/java/com/GDTW/urlSafetyCheck/model/UrlSafetyCheckService.java
@@ -1,13 +1,11 @@
 package com.GDTW.urlSafetyCheck.model;
 
 import com.GDTW.safebrowsing4.service.SafeBrowsingV4Service;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UrlSafetyCheckService {
+
     private final SafeBrowsingV4Service safeBrowsingV4Service;
 
     public UrlSafetyCheckService(SafeBrowsingV4Service safeBrowsingV4Service) {
