4191d66de0a8b0e6dbe10cddd36b571c6cb7ede1
eugen.lin <eugen.lin@raritan.com>
2025-05-08 10:12:44 +0800

    2025May08 v1.15.0 1. To upgrade all front end resources.

--- src/main/java/com/GDTW/general/service/ratelimiter/RateLimiterService.java
+++ src/main/java/com/GDTW/general/service/ratelimiter/RateLimiterService.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
