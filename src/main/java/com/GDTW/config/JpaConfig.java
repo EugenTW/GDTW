6c19da79dfeffe67eee1ef55b10e42961521290d
eugen.lin <eugen.lin@raritan.com>
2025-05-08 10:48:47 +0800

    2025May08 v1.15.0 1. To upgrade all front end resources.

--- src/main/java/com/GDTW/config/JpaConfig.java
+++ src/main/java/com/GDTW/config/JpaConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.GDTW")
+@EnableJpaRepositories(basePackages = {
+        "com.GDTW.dailystatistic.model",
+        "com.GDTW.imgshare.model",
+        "com.GDTW.shorturl.model"
+})
 public class JpaConfig {
 }
