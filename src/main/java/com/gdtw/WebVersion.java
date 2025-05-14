ce69c14ecab5e15599d7fffba41fe263d927f44b
eugen.lin <eugen.lin@raritan.com>
2025-05-14 11:48:59 +0800

    2025May14 v1.15.3 1. To update front-end resources.

--- src/main/java/com/gdtw/WebVersion.java
+++ src/main/java/com/gdtw/WebVersion.java
@@ -3,7 +3,7 @@ package com.gdtw;
 public class WebVersion {
 
     private static final String WEB_VERSION = "1.15.3";
-    private static final String BUILD_DATE = "2025May13";
+    private static final String BUILD_DATE = "2025May14";
 
     private WebVersion(){}
 
