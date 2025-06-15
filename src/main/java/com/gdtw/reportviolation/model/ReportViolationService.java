a5dff69ca50417880868ff8f6fc3d322e1e152bc
EugenPC-IDEA <hsuanyulin1993@gmail.com>
2025-06-15 11:04:22 +0800

    2025Jun15 v1.17.5

--- src/main/java/com/gdtw/reportviolation/model/ReportViolationService.java
+++ src/main/java/com/gdtw/reportviolation/model/ReportViolationService.java
@@ -5,12 +5,7 @@ import com.gdtw.general.util.CodecImgIdUtil;
 import com.gdtw.imgshare.model.ImgShareService;
 import com.gdtw.reportviolation.dto.ReportRequestDTO;
 import com.gdtw.shorturl.model.ShortUrlService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
@@ -19,7 +14,6 @@ import java.util.Map;
 @Service
 public class ReportViolationService {
 
-    private static final Logger logger = LoggerFactory.getLogger(ReportViolationService.class);
     private final ReportViolationPersistenceService reportViolationPersistenceService;
     private final ShortUrlService shortUrlService;
     private final ImgShareService imgShareService;
@@ -34,45 +28,37 @@ public class ReportViolationService {
     public Map<String, String> createViolationReport(ReportRequestDTO dto, String originalIp) {
         Map<String, String> result = new HashMap<>();
 
-        try {
-            String reportedUrl = dto.getTargetUrl();
-            int reportedType = dto.getReportType();
+        String reportedUrl = dto.getTargetUrl();
+        int reportedType = dto.getReportType();
 
-            reportViolationPersistenceService.saveReportViolationTransactional(dto, originalIp);
+        reportViolationPersistenceService.saveReportViolationTransactional(dto, originalIp);
 
-            switch (reportedType) {
-                case 1:
-                    Integer shortUrlId = CodecShortUrlIdUtil.decodeId(reportedUrl);
-                    shortUrlService.reportShortUrl(shortUrlId, result);
-                    break;
-                case 2:
-                    Integer albumId = CodecImgIdUtil.decodeImgId(reportedUrl);
-                    imgShareService.reportImgAlbum(albumId, result);
-                    break;
-                case 3:
-                    Integer imageId = CodecImgIdUtil.decodeImgId(reportedUrl);
-                    imgShareService.reportImage(imageId, result);
-                    break;
-                default:
-                    result.put("reportStatus", "false");
-                    result.put("response", "非定義的'舉報種類'！\nUndefined 'Reported Type'!");
-                    return result;
-            }
-
-            if (!result.containsKey("reportStatus")) {
+        switch (reportedType) {
+            case 1:
+                Integer shortUrlId = CodecShortUrlIdUtil.decodeId(reportedUrl);
+                shortUrlService.reportShortUrl(shortUrlId, result);
+                break;
+            case 2:
+                Integer albumId = CodecImgIdUtil.decodeImgId(reportedUrl);
+                imgShareService.reportImgAlbum(albumId, result);
+                break;
+            case 3:
+                Integer imageId = CodecImgIdUtil.decodeImgId(reportedUrl);
+                imgShareService.reportImage(imageId, result);
+                break;
+            default:
                 result.put("reportStatus", "false");
-                result.put("response", "舉報失敗，請稍後再試。\nReport failed, please try again.");
-            }
-            return result;
-        } catch (UnexpectedRollbackException ex) {
-            result.put("reportStatus", "false");
-            result.put("response", "您已經針對此資源舉報過，不能重複舉報。\nYou have already reported this resource and cannot report again.");
-            return result;
-        } catch (Exception ex) {
-            result.put("reportStatus", "false");
-            result.put("response", "伺服器錯誤，請稍後再試。\nServer error, please try again. (" + ex.getClass().getSimpleName() + ")");
-            return result;
+                result.put("response", "非定義的'舉報種類'！<br>Undefined 'Reported Type'!");
+                return result;
         }
+
+        if (!result.containsKey("reportStatus")) {
+            result.put("reportStatus", "false");
+            result.put("response", "舉報失敗，請稍後再試。<br>Report failed, please try again.");
+        }
+        return result;
     }
 
 }
+
+
