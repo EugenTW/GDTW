a34ea08558e2439df9b8a309a62ba46de7c5f147
eugen.lin <eugen.lin@raritan.com>
2025-06-26 13:46:03 +0800

    2025Jun26 v1.17.7

--- src/main/java/com/gdtw/WebVersion.java
+++ src/main/java/com/gdtw/WebVersion.java
@@ -2,8 +2,8 @@ package com.gdtw;
 
 public class WebVersion {
 
-    private static final String WEB_VERSION = "1.17.6";
-    private static final String BUILD_DATE = "2025Jun21";
+    private static final String WEB_VERSION = "1.17.7";
+    private static final String BUILD_DATE = "2025Jun26";
 
     private WebVersion(){}
 
