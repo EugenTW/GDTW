ad866da4849d0f7ed3cfa775c3b89f41f139d033
eugen.lin <eugen.lin@raritan.com>
2025-10-21 13:44:57 +0800

    2025Oct21 v1.20.3 1. To fix bugs

--- src/main/java/com/gdtw/WebVersion.java
+++ src/main/java/com/gdtw/WebVersion.java
@@ -3,7 +3,7 @@ package com.gdtw;
 public class WebVersion {
 
     private static final String WEB_VERSION = "1.20.3";
-    private static final String BUILD_DATE = "2025Oct17";
+    private static final String BUILD_DATE = "2025Oct21";
 
     private WebVersion(){}
 
